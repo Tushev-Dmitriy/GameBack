add9248fcf509deb1a8b0c6fb4cc26e89836ccd1
Tushev-Dmitriy <110030309+Tushev-Dmitriy@users.noreply.github.com>
2024-12-08 21:57:43 +0100

    update for host

--- app/main.py
+++ app/main.py
@@ -10,5 +10,9 @@ app.include_router(works.router, prefix="/works", tags=["works"])
 app.include_router(users.router, prefix="/user", tags=["user"])
 app.include_router(rooms.router, prefix="/room", tags=["room"])
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Server!"}
+
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
